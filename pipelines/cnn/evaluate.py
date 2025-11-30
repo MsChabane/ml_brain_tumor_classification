dda8d5e67e501af09e88fc2395a41e5dd1264f69
chabane <messouschabane77@gmail.com>
2025-11-30 10:02:04 +0100

    evaluate the model cnn

---
+++ pipelines/cnn/evaluate.py
@@ -0,0 +1,42 @@
+from sklearn.metrics import confusion_matrix,accuracy_score,precision_score,recall_score,f1_score,roc_curve, auc
+import numpy as np
+from json import dump
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def evaluate(model,test_data):
+    proba=model.predict(test_data).flatten()
+    ypreds=np.where(proba>0.5,1,0)
+
+    fpr, tpr, thresholds = roc_curve(test_data.classes, proba)
+    result={
+    'accuracy':accuracy_score(test_data.classes,ypreds),
+    'precision':precision_score(test_data.classes,ypreds),
+    'recall':recall_score(test_data.classes,ypreds),
+    'f1_score':f1_score(test_data.classes,ypreds),
+    'auc':auc(fpr, tpr)
+    }
+    with open('./reports/cnn/result.json','w') as f:
+        dump(result,f)
+    
+    cm = confusion_matrix(test_data.classes, ypreds)
+    plt.figure(figsize=(8,6))
+    sns.heatmap(cm,annot=True,fmt='d',cmap='Blues')
+    plt.xlabel('Predicted')
+    plt.ylabel('True')
+    plt.title('Confusion Matrix')
+    plt.savefig('./reports/cnn/confusion_matrix.png')
+    plt.figure(figsize=(8,6))
+    plt.plot(fpr, tpr, color='darkorange', lw=2)
+    plt.plot([0,1], [0,1], color='navy', lw=2, linestyle='--')  
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic (ROC)')
+    plt.legend(loc="lower right")
+    plt.show()
+    plt.savefig('./reports/cnn/roc_curve.png')
+
+
+
