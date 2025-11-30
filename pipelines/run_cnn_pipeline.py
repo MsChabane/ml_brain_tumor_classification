2dd487a1090e51fea0f4b3af8a8105013089eac0
chabane <messouschabane77@gmail.com>
2025-11-30 11:16:29 +0100

    run the cnn pipeline

--- pipelines/run_cnn_pipeline.py
+++ pipelines/run_cnn_pipeline.py
@@ -1,9 +1,26 @@
-
-
+from cnn.evaluate import evaluate
+from cnn.load_data import load_data_to
+from cnn.prepare import prepare
+from cnn.train import train
+import os
 
 def run_pipeline():
     print("pipeline is running...")
-    pass
+    print(">> loading the data... ")
+    path='./data'
+    os.makedirs(path,exist_ok=True)
+    load_data_to(path)
+    print('>> prepare the data ...')
+    train_path=os.path.join(path,'Brain_Tumor_Datasets','train')
+    test_path=os.path.join(path,'Brain_Tumor_Datasets','test')
+    train_data,val_data,test_data= prepare(train_path,test_path)
+    print('>> train the model ...')
+    model = train(train_data,val_data,checkpoint_path='./models/cnn/model.keras')
+    
+    print('>> evalate the model ...')
+    evaluate(model,test_data)
+    print("pipeline is completed ")
+    
 
 
 
